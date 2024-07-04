a59b51f346850870839c372c9a53259e1c08d165
cubedhuang <99danielh@gmail.com>
2024-07-03 21:11:22 -0400

    add explanations

---
+++ src/lib/Explanation.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	import type { AnswerContext } from './context';
+	import { slide } from 'svelte/transition';
+
+	const { answer, guess } = getContext<AnswerContext>('answers');
+
+	$: open = $answer !== '';
+</script>
+
+{#if open}
+	<div transition:slide>
+		<slot />
+	</div>
+{/if}
